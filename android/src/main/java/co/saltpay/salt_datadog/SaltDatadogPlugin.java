93c58a35642a361979d908ee68dcf4303cb1d4d6
Alexander Demin <alexander@demin.ws>
2021-01-14 15:42:39 +0000

    Remove comments

--- android/src/main/java/co/saltpay/salt_datadog/SaltDatadogPlugin.java
+++ android/src/main/java/co/saltpay/salt_datadog/SaltDatadogPlugin.java
@@ -25,10 +25,6 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
  * SaltDatadogPlugin
  */
 public class SaltDatadogPlugin implements FlutterPlugin, MethodCallHandler {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
     private FlutterPluginBinding flutterPluginBinding;
 
